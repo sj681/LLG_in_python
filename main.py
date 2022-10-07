3d61a2fb27d6cd87aaf77bc1432358933bf45e0a
george <george.duffett@icloud.com>
2022-10-07 18:04:56 +0100

    Change to use separate spin file

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
 import numpy as np
 from typing import NamedTuple
 
+from src.spin import Spin
 
 class Material(NamedTuple):
     spin_moment: float
     exchange_between_i_and_j: float
     anisotropy_i: float
 
-
-class Spin(object):
-
-    def __init__(self, position_x,position_y, position_z, spin_position_x, spin_position_y,spin_position_z,type_of_material,anisotropy,spin_moment):
-
-        self.position_x = position_x
-        self.position_y = position_y
-        self.position_z = position_z
-        self.spin_position_x = spin_position_x
-        self.spin_position_y = spin_position_y
-        self.spin_position_z = spin_position_z
-        self.type_of_material = type_of_material
-        self.anisotropy = anisotropy
-        self.spin_moment = spin_moment
 
 def calculate_fields(field):
     field = [0,0,0]
