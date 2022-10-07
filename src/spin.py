998a98f3b05430b9f280b9b2d94306c22d13d8d8
george <george.duffett@icloud.com>
2022-10-07 18:04:43 +0100

    Add separate spin file

---
+++ src/spin.py
@@ -0,0 +1,13 @@
+class Spin(object):
+
+    def __init__(self, position_x,position_y, position_z, spin_position_x, spin_position_y,spin_position_z,type_of_material,anisotropy,spin_moment):
+
+        self.position_x = position_x
+        self.position_y = position_y
+        self.position_z = position_z
+        self.spin_position_x = spin_position_x
+        self.spin_position_y = spin_position_y
+        self.spin_position_z = spin_position_z
+        self.type_of_material = type_of_material
+        self.anisotropy = anisotropy
+        self.spin_moment = spin_moment
