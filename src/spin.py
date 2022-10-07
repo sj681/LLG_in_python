60c33d94d9247c375634b2f9ec7f4376601bd0c2
george <george.duffett@icloud.com>
2022-10-07 18:12:34 +0100

    Change spin constructor whitespace

--- src/spin.py
+++ src/spin.py
@@ -1,6 +1,6 @@
 class Spin(object):
 
-    def __init__(self, position_x,position_y, position_z, spin_position_x, spin_position_y,spin_position_z,type_of_material,anisotropy,spin_moment):
+    def __init__(self, position_x, position_y, position_z, spin_position_x, spin_position_y, spin_position_z, type_of_material, anisotropy, spin_moment):
 
         self.position_x = position_x
         self.position_y = position_y
